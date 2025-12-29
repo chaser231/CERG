0d5eb9e7489fb35a699bbfb44e499721405bbd02
CERG Developer <dev@cerg.ru>
2025-12-29 21:14:16 +0300

    fix: add base URL prefix to all image paths for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,8 @@ import preact from '@astrojs/preact';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://chaser231.github.io',
+  base: '/CERG',
   vite: {
     plugins: [tailwindcss()]
   },
